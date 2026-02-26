d881bf15628fcf20ad5c8292a7352389ee3ae9f1
qifeng22 <57132534+qifeng22@users.noreply.github.com>
2026-02-26 14:09:40 +0800

    once

--- pcdet/models/backbones_3d/lion_backbone_one_stride.py
+++ pcdet/models/backbones_3d/lion_backbone_one_stride.py
@@ -761,6 +761,148 @@ class LION3DBackboneOneStride(nn.Module):
         return batch_dict
 
 
+#for  once
+class LION3DBackboneOneStride_once(nn.Module):
+    def __init__(self, model_cfg, input_channels, grid_size, **kwargs):
+        super().__init__()
+
+        self.model_cfg = model_cfg
+
+        self.sparse_shape = grid_size[::-1]  # + [1, 0, 0]
+        norm_fn = partial(nn.LayerNorm)
+
+        dim = model_cfg.FEATURE_DIM
+        num_layers = model_cfg.NUM_LAYERS
+        depths = model_cfg.DEPTHS
+        layer_down_scales = model_cfg.LAYER_DOWN_SCALES
+        direction = model_cfg.DIRECTION
+        diffusion = model_cfg.DIFFUSION
+        shift = model_cfg.SHIFT
+        diff_scale = model_cfg.DIFF_SCALE
+        self.window_shape = model_cfg.WINDOW_SHAPE
+        self.group_size = model_cfg.GROUP_SIZE
+        self.layer_dim = model_cfg.LAYER_DIM
+        self.linear_operator = model_cfg.OPERATOR
+        
+        self.n_layer = len(depths) * depths[0] * 2 * 2 + 2
+
+        down_scale_list = [[2, 2, 2],
+                           [2, 2, 2],
+                           [2, 2, 1],
+                           [1, 1, 2],
+                           [1, 1, 2]
+                           ]
+        total_down_scale_list = [down_scale_list[0]]
+        for i in range(len(down_scale_list) - 1):
+            tmp_dow_scale = [x * y for x, y in zip(total_down_scale_list[i], down_scale_list[i + 1])]
+            total_down_scale_list.append(tmp_dow_scale)
+
+        assert num_layers == len(depths)
+        assert len(layer_down_scales) == len(depths)
+        assert len(layer_down_scales[0]) == depths[0]
+        assert len(self.layer_dim) == len(depths)
+
+        self.stem = spconv.SparseSequential(
+            post_act_block_sparse_3d(input_channels, 16, 3, 1, 1, indice_key='subm1', conv_type='subm'),
+
+            SparseBasicBlock3D(16, indice_key='conv1'),
+            SparseBasicBlock3D(16, indice_key='conv1'),
+            post_act_block_sparse_3d(16, 32, 3, (1,2,2), 1, indice_key='spconv1', conv_type='spconv'),
+
+            SparseBasicBlock3D(32, indice_key='conv2'),
+            SparseBasicBlock3D(32, indice_key='conv2'),
+            post_act_block_sparse_3d(32, self.layer_dim[0], 3, (1,2,2), 1, indice_key='spconv2', conv_type='spconv'),)
+
+
+        
+        self.linear_1 = LIONBlock(self.layer_dim[0], depths[0], layer_down_scales[0], self.window_shape[0],
+                                    self.group_size[0], direction, shift=shift, operator=self.linear_operator, layer_id=0, n_layer=self.n_layer)  ##[27, 27, 32] --》 [13, 13, 32]
+
+        self.dow1 = PatchMerging3D(self.layer_dim[0], self.layer_dim[0], down_scale=[1, 1, 2],
+                                     norm_layer=norm_fn, diffusion=diffusion, diff_scale=diff_scale)
+        
+
+        # [944, 944, 16] -> [472, 472, 8]
+        self.linear_2 = LIONBlock(self.layer_dim[1], depths[1], layer_down_scales[1], self.window_shape[1],
+                                    self.group_size[1], direction, shift=shift, operator=self.linear_operator, layer_id=8, n_layer=self.n_layer)
+
+        self.dow2 = PatchMerging3D(self.layer_dim[1], self.layer_dim[1], down_scale=[1, 1, 2],
+                                     norm_layer=norm_fn, diffusion=diffusion, diff_scale=diff_scale)
+
+
+        #  [236, 236, 8] -> [236, 236, 4]
+        self.linear_3 = LIONBlock(self.layer_dim[2], depths[2], layer_down_scales[2], self.window_shape[2],
+                                    self.group_size[2], direction, shift=shift, operator=self.linear_operator, layer_id=16, n_layer=self.n_layer)
+
+        self.dow3 = PatchMerging3D(self.layer_dim[2], self.layer_dim[3], down_scale=[1, 1, 2],
+                                     norm_layer=norm_fn, diffusion=diffusion, diff_scale=diff_scale)
+
+        #  [236, 236, 4] -> [236, 236, 2]
+        self.linear_4 = LIONBlock(self.layer_dim[3], depths[3], layer_down_scales[3], self.window_shape[3],
+                                    self.group_size[3], direction, shift=shift, operator=self.linear_operator, layer_id=24, n_layer=self.n_layer)
+
+        self.dow4 = PatchMerging3D(self.layer_dim[3], self.layer_dim[3], down_scale=[1, 1, 2],
+                                     norm_layer=norm_fn, diffusion=diffusion, diff_scale=diff_scale)
+
+        self.linear_out = LIONLayer(self.layer_dim[3], 1, [13, 13, 2], 256, direction=['x', 'y'], shift=shift,
+                                      operator=self.linear_operator, layer_id=32, n_layer=self.n_layer)
+
+        self.num_point_features = dim
+
+        self.backbone_channels = {
+            'x_conv1': 128,
+            'x_conv2': 128,
+            'x_conv3': 128,
+            'x_conv4': 128
+        }
+
+    def forward(self, batch_dict):
+        voxel_features = batch_dict['voxel_features']
+        voxel_coords = batch_dict['voxel_coords']
+        batch_size = batch_dict['batch_size']
+
+        x = spconv.SparseConvTensor(
+            features=voxel_features,
+            indices=voxel_coords.int(),
+            spatial_shape=self.sparse_shape,
+            batch_size=batch_size
+        )
+        x =self.stem(x)
+        x = self.linear_1(x)
+        x1, _ = self.dow1(x)  ## 14.0k --> 16.9k  [32, 1000, 1000]-->[16, 1000, 1000]
+        x = self.linear_2(x1)
+        x2, _ = self.dow2(x)  ## 16.9k --> 18.8k  [16, 1000, 1000]-->[8, 1000, 1000]
+        x = self.linear_3(x2)
+        x3, _ = self.dow3(x)   ## 18.8k --> 19.1k  [8, 1000, 1000]-->[4, 1000, 1000]
+        x = self.linear_4(x3)
+        x4, _ = self.dow4(x)  ## 19.1k --> 18.5k  [4, 1000, 1000]-->[2, 1000, 1000]
+        # print(x4.spatial_shape)
+        x = self.linear_out(x4)
+        # print(x.spatial_shape,'ffff')
+        batch_dict.update({
+            'encoded_spconv_tensor': x,
+            'encoded_spconv_tensor_stride': 1
+        })
+
+        batch_dict.update({
+            'multi_scale_3d_features': {
+                'x_conv1': x1,
+                'x_conv2': x2,
+                'x_conv3': x3,
+                'x_conv4': x4,
+            }
+        })
+        batch_dict.update({
+            'multi_scale_3d_strides': {
+                'x_conv1': torch.tensor([1,1,2], device=x1.features.device).float(),
+                'x_conv2': torch.tensor([1,1,4], device=x1.features.device).float(),
+                'x_conv3': torch.tensor([1,1,8], device=x1.features.device).float(),
+                'x_conv4': torch.tensor([1,1,16], device=x1.features.device).float(),
+            }
+        })
+
+        return batch_dict
+
 
 #for argoverse
 class LION3DBackboneOneStride_Sparse(nn.Module):
